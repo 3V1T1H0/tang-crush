ad2dcddcabe693d6fb859f980b3eb595c84082e4
3V1T1H0 <87942683+3V1T1H0@users.noreply.github.com>
2021-07-25 23:38:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-     = {
+     const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Lý trí nói quên nhưng ...
-    tim nói nhớ`,
+    introDesc: `Lý trí nói quên nhưng...
+                tim nói nhớ `,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'ta còn cơ hội mà 🥰',
-    btnNo: 'Thôi, FRIENDZONE  :3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: ' Ta còn cơ hội mà <33',
+    btnNo: 'Không, FRIENDZONE thôi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Nhắn cho anh nhé công chúa.',
+    messDesc: 'nhắn cho anh luôn đi công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/giang.ai.98/' 
-}
+    messLink: 'https://www.messenger.com/giang.ai.98/'
